8732cfabe77cf9d76f057412ac8122099c0920bd
Hazy Cora <hazycora@gmail.com>
2021-02-24 13:33:50 -0600

    add proxy button

--- index.js
+++ index.js
@@ -99,7 +99,7 @@ async function requestListener(request, response) {
 
                                 // web result adding
                                 for (var c in res.results) {
-                                    var chip = "<a class='resLink' href='" + res.results[c].url + "'><div class='result'><h2>" + res.results[c].title + "</h2><div class='urlCont'><img class='favicon' src='/favicon/?link=" + btoa(res.results[c].url) + "'><h4>" + res.results[c].url + "</h4></div><p>" + res.results[c].description + "</p></div></a>";
+                                    var chip = "<a class='resLink' href='" + res.results[c].url + "'><div class='result'><a href='proxy?url=" + btoa(res.results[c].url) + "'><img src='globe.png' class='proxyImage'></img></a><h2>" + res.results[c].title + "</h2><div class='urlCont'><img class='favicon' src='/favicon/?link=" + btoa(res.results[c].url) + "'><h4>" + res.results[c].url + "</h4></div><p>" + res.results[c].description + "</p></div></a>";
                                     $(".main").append(chip);
                                 }
 
